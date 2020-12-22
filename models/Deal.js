c16b9fbe4bd8b25cd93496f826e8b6f307873f68
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-12-23 01:12:20 +0700

    doing -.-

--- models/Deal.js
+++ models/Deal.js
@@ -1,8 +1,7 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 /* DealSchema will correspond to a collection in your MongoDB database. */
 const DealSchema = new mongoose.Schema({
-  
   list_food: {
     type: Object,
     /*
@@ -11,20 +10,21 @@ const DealSchema = new mongoose.Schema({
         amount: ...,
       }
     */
-  }, 
+  },
   id_user: {
-    type: String, /* id_user */
-  }, 
+    type: String /* id_user */,
+  },
   typeDeal: {
-    type: String, /* 3 trạng thái: pending, accept, refuse */
-  }, 
+    type: String /* 3 trạng thái: pending, accept, refuse */,
+  },
   created: {
-    type: String, /*Thời gian tạo deal */
+    type: String /*Thời gian tạo deal */,
   },
   totalPrice: {
     type: Number,
   },
+  id_account: { type: String },
+  deal: { type: Object },
+});
 
-})
-
-export default mongoose.models.Deal || mongoose.model('Deal', DealSchema)
+export default mongoose.models.Deal || mongoose.model("Deal", DealSchema);
--- pages/api/deals/index.js
+++ pages/api/deals/index.js
@@ -1,33 +1,32 @@
-import dbConnect from '../../../utils/dbConnect'
-import Deal from '../../../models/Deal'
+import Deal from "../../../models/Deal";
+import dbConnect from "../../../utils/dbConnect";
 
 export default async function handler(req, res) {
-  const { 
-    method,
-  } = req
-  await dbConnect()
+  const { method } = req;
+  await dbConnect();
 
   switch (method) {
-    case 'GET':
+    case "GET":
       try {
-        const deals = await Deal.find() /* find all the data in our database */
-        res.status(200).json({ success: true, data: deals })
+        const deals = await Deal.find(); /* find all the data in our database */
+        res.status(200).json({ success: true, data: deals });
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).json({ success: false });
       }
-      break
-    case 'POST':
+      break;
+    case "POST":
       try {
+        console.log("req.body :>> ", req.body);
         const deal = await Deal.create(
           req.body
-        ) /* create a new model in the database */
-        res.status(201).json({ success: true, data: deal })
+        ); /* create a new model in the database */
+        res.status(201).json({ success: true, data: deal });
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).json({ success: false });
       }
-      break
+      break;
     default:
-      res.status(400).json({ success: false })
-      break
+      res.status(400).json({ success: false });
+      break;
   }
 }
