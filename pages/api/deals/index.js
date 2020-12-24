07023dfb943e3286823a386f6343499bbb9cf769
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-12-25 00:12:36 +0700

    doing :||

--- pages/api/deals/index.js
+++ pages/api/deals/index.js
@@ -2,13 +2,18 @@ import Deal from "../../../models/Deal";
 import dbConnect from "../../../utils/dbConnect";
 
 export default async function handler(req, res) {
-  const { method } = req;
+  const {
+    query: { id_account },
+    method,
+  } = req;
   await dbConnect();
 
   switch (method) {
     case "GET":
       try {
-        const deals = await Deal.find(); /* find all the data in our database */
+        const deals = await Deal.find({
+          id_account,
+        }); /* find all the data in our database */
         res.status(200).json({ success: true, data: deals });
       } catch (error) {
         res.status(400).json({ success: false });
@@ -16,7 +21,6 @@ export default async function handler(req, res) {
       break;
     case "POST":
       try {
-        console.log("req.body :>> ", req.body);
         const deal = await Deal.create(
           req.body
         ); /* create a new model in the database */
